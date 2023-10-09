74fa55e22e9a90e41121ed982d877ff6cb300750
Resent <remal11151@gmail.com>
2023-10-09 14:12:26 +0300

    Added SDL2 Image and Mixer Extensions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
 
 int main(int argc, char *args[]) {
     // Init SDL module
@@ -16,7 +19,7 @@ int main(int argc, char *args[]) {
     };
 
     //Creating window
-    SDL_Window *win = SDL_CreateWindow("Title re", 0, 0, displayMode.w, displayMode.h, SDL_WINDOW_SHOWN);
+    SDL_Window *win = SDL_CreateWindow("Title re", 0, 0, displayMode.w, displayMode.h, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     if (win == nullptr) {
         std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         return 1;
@@ -27,5 +30,24 @@ int main(int argc, char *args[]) {
         std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         return 1;
     }
+
+    SDL_Rect player_RECT;
+    player_RECT.x = 0;   //Padding in Х
+    player_RECT.y = 0;   //Padding in Y
+    player_RECT.w = 333; //Width
+    player_RECT.h = 227; //Height
+
+    SDL_Rect background_RECT;
+    background_RECT.x = 0;
+    background_RECT.y = 0;
+    background_RECT.w = displayMode.w;
+    background_RECT.h = displayMode.h;
+
+    const int player_WIGHT = 333;   //Width of img
+    const int player_HEIGH = 227;   //Height of img
+    double TESTtexture_SCALE = 1.0; //Coefficient
+
+    SDL_Texture *player =  IMG_LoadTexture(ren,"..\\res\\player.png");
+
     return 0;
 }
